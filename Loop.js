a8ab08b00825d5f33d207341ef5404b05c7842e2
NishaAshokan <madhinishaashokan207@gmail.com>
2023-12-19 00:20:38 +0530

    resumeData

--- Loop.js
+++ Loop.js
@@ -28,6 +28,63 @@ let jsonArray = '{ "numbers" : ["1", "2", "3", "4", "5", "6"]}';
 // for(newNum of normalArray){
 //     console.log(newNum);
 // }
-normalArray.forEach(element => {
-    console.log(element);
-});
+// normalArray.forEach(element => {
+//     console.log(element);
+// });
+
+// let jsonResume = '{
+//     "Name" : "Madhinishanthi",
+//     "Age " : "23" 
+
+// }'; 
+// let jsonObject = JSON.parse(jsonResume);
+// let normalResume = jsonObject.Name;
+// console.log(normalResume);
+
+const resumeData = {
+    "personalInfo": {
+      "FirstName": "Madhinishanthi",
+      "LastName": "Ashokan",
+      "Email": "madhinishaashokan207@gmail.com",
+      "Phone": "6938958585",
+      "Address": "12,Street, city, district, state, Country"
+    },
+    "education": [
+      {
+        "Degree": "B.TECH BIOTECHNOLOGY",
+        "School": "SCHOOL OF SOMETHING",
+        "GraduationYear": 2021
+      }
+    ],
+    "Experience": [
+      {
+        "Position": "SPI ENGINEER",
+        "Company": "GHJNM.",
+        "StartDate": "JULY 2023",
+        "EndDate": "Present",
+        "Responsibilities": [
+          "Working in smart plant instrumentation",
+          "working in different module",
+          "Datasheets"
+        ]
+      }
+    ],
+    "skills": [
+      "JavaScript",
+      "Java",
+      "Mysql",
+      "React.js",
+      "Node.js",
+      "HTML",
+      "CSS",
+      "Git"  
+    ],
+    "languages": [
+      "English (Fluent)",
+      "Japanese (Beginner)"
+    ]
+  };
+  
+  // Print the resume data
+  console.log(JSON.stringify(resumeData, null, 2));
+  
